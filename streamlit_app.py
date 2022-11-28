e69eb45891c3c6ada1f5aa40e413ef7d54beae5c
ChrisWooding <109672362+ChrisWooding@users.noreply.github.com>
2022-11-28 10:50:59 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.heading('Breakfast Menu')
+
+streamlit.print('Omega 3 and blueberry oatmeal')
+streamlit.print('Kale, spinach & rocket smoothie')
+streamlit.print('Hard-boiled free-range egg')
