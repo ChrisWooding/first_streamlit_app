4a05ed46e45f358cfa90a9347ea2574af09aa8ec
ChrisWooding <109672362+ChrisWooding@users.noreply.github.com>
2022-11-28 10:58:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 and blueberry oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard-boiled free-range egg')
+streamlit.text(🥣'Omega 3 and blueberry oatmeal'🥣)
+streamlit.text(🥗'Kale, spinach & rocket smoothie'🥗)
+streamlit.text(🐔'Hard-boiled free-range egg'🐔)
+streamlit.text(🥑🍞'Avocado toast')
